d6fd57f64a003237af0eea8d8ec890b2934c9d13
Julian <jaykmcclinton@gmail.com>
2014-11-05 09:29:25 -0500

    More android specific
    This project is a gradle build, which means it uses multiple projects to
    work. You WILL need the gradle plug-in for eclipse for this. The point
    is to make it easier to code for android, since the sdk itself is very
    specific and uncooperative

---
+++ android/src/edu/virginia/CS2110/ghostgame/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package edu.virginia.CS2110.ghostgame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import edu.virginia.CS2110.ghostgame.GhostGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GhostGame(), config);
+	}
+}
