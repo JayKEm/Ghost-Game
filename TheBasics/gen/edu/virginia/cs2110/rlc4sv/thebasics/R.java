c00e1f3b67f61f1c4cb6b6d5792209cf483dffbb
AlexBailey20 <arb4jr@virginia.edu>
2014-11-11 19:17:22 -0500

    Different menu background

--- TheBasics/gen/edu/virginia/cs2110/rlc4sv/thebasics/R.java
+++ TheBasics/gen/edu/virginia/cs2110/rlc4sv/thebasics/R.java
@@ -33,16 +33,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int background1=0x7f020000;
-        public static final int background2=0x7f020001;
-        public static final int background3=0x7f020002;
-        public static final int blueball=0x7f020003;
-        public static final int directions=0x7f020004;
-        public static final int explode=0x7f020005;
-        public static final int gspritesheet=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int spritesheet=0x7f020008;
-        public static final int white_background2=0x7f020009;
+        public static final int background3=0x7f020000;
+        public static final int directions=0x7f020001;
+        public static final int explode=0x7f020002;
+        public static final int ghostbackground=0x7f020003;
+        public static final int gspritesheet=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int spritesheet=0x7f020006;
+        public static final int white_background2=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0015;
